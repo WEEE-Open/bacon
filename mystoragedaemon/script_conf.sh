c615d2028caac8a929bd8b829cf1f0715cc3c9f7
campa <mac.aerdna@gmail.com>
2021-04-20 11:25:14 +0200

    sd path now env var

--- mystoragedaemon/script_conf.sh
+++ mystoragedaemon/script_conf.sh
@@ -6,3 +6,6 @@ sed -i "s/{{SDPASS}}/$SDPASS/g" /etc/bacula/bacula-sd.conf
 
 echo -e "\e[1;46m ++++++++++ Replacing passwords in Bacula Director config ++++++++++ \e[0m"
 sed -i "s/{{SDADDR}}/$SDADDR/g" /etc/bacula/bacula-sd.conf
+
+echo -e "\e[1;46m ++++++++++ Replacing dir where to place backups in Bacula Director config ++++++++++ \e[0m"
+sed -i "s/{{DESTINATION}}/$DESTINATION/g" /etc/bacula/bacula-sd.conf

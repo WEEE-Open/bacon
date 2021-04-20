3093f277ddb7c3bbdcdd9ad643e24ba2b2af0511
campa <mac.aerdna@gmail.com>
2021-04-20 11:44:10 +0200

    clearer env var names and corrected some comments

--- myfiledaemon/script_conf.sh
+++ myfiledaemon/script_conf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # this script is changes the defaults of the filedaemon
 
-echo -e "\e[1;46m ++++++++++ Replacing passwords in Bacula Director config ++++++++++ \e[0m"
+echo -e "\e[1;46m ++++++++++ Replacing passwords in Bacula file daemon config ++++++++++ \e[0m"
 sed -i "s/{{FDPASS}}/$FDPASS/g" /etc/bacula/bacula-fd.conf
 
-echo -e "\e[1;46m ++++++++++ Replacing addr in Bacula Director config ++++++++++ \e[0m"
+echo -e "\e[1;46m ++++++++++ Replacing addr in Bacula file daemon config ++++++++++ \e[0m"
 sed -i "s/{{FDADDR}}/$FDADDR/g" /etc/bacula/bacula-fd.conf
--- mystoragedaemon/script_conf.sh
+++ mystoragedaemon/script_conf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # this script is changes the defaults of the storagedaemon
 
-echo -e "\e[1;46m ++++++++++ Replacing passwords in Bacula Director config ++++++++++ \e[0m"
+echo -e "\e[1;46m ++++++++++ Replacing passwords in Bacula storage daemon config ++++++++++ \e[0m"
 sed -i "s/{{SDPASS}}/$SDPASS/g" /etc/bacula/bacula-sd.conf
 
-echo -e "\e[1;46m ++++++++++ Replacing passwords in Bacula Director config ++++++++++ \e[0m"
+echo -e "\e[1;46m ++++++++++ Replacing passwords in Bacula storage daemon config ++++++++++ \e[0m"
 sed -i "s/{{SDADDR}}/$SDADDR/g" /etc/bacula/bacula-sd.conf
 
-echo -e "\e[1;46m ++++++++++ Replacing dir where to place backups in Bacula Director config ++++++++++ \e[0m"
-sed -i "s/{{DESTINATION}}/$DESTINATION/g" /etc/bacula/bacula-sd.conf
+echo -e "\e[1;46m ++++++++++ Replacing directories in Bacula storage daemon config ++++++++++ \e[0m"
+sed -i "s/{{SDDIR}}/$SDDIR/g" /etc/bacula/bacula-sd.conf
